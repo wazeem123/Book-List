7ec066b3f623897b5bd807729bbc75f9170fb4c4
wazeem123 <mwazeem84@gmail.com>
2020-12-13 15:33:37 +0530

    Updates

---
+++ precache-manifest.c59560e636deb3a7b00b8beae1275a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac61772f0983018948babbae0ef19596",
+    "url": "/Book-List/index.html"
+  },
+  {
+    "revision": "292a194ca65c35aed85f",
+    "url": "/Book-List/static/css/main.dd860af4.chunk.css"
+  },
+  {
+    "revision": "a22b075fc23875175ef2",
+    "url": "/Book-List/static/js/2.91b1f3f0.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/Book-List/static/js/2.91b1f3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292a194ca65c35aed85f",
+    "url": "/Book-List/static/js/main.21a618d6.chunk.js"
+  },
+  {
+    "revision": "327cabc73dbf7d814bc2",
+    "url": "/Book-List/static/js/runtime-main.d899e2fe.js"
+  }
+]);
